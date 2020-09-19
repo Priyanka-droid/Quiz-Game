b8058ea311c8e464766c52c97335b539c235e9a8
Priyanka-droid <55012840+Priyanka-droid@users.noreply.github.com>
2020-09-20 00:17:58 +0530

    Create my_quizgame.cpp

---
+++ my_quizgame.cpp
@@ -0,0 +1,311 @@
+#include<stdio.h>
+#include<conio.h>
+#include<ctype.h>
+#include<stdlib.h>
+#include<string.h>
+void show_record();
+void reset_score();
+void help();
+void edit_score(float , char []);
+int main()
+     {
+     int countr,r,r1,count,i,n;
+     float score;
+     char choice;
+     char playername[20];
+     mainhome:
+     system("cls");
+     printf("\t\t\tC PROGRAM QUIZ GAME\n");
+     printf("\n\t\t________________________________________");
+
+     printf("\n\t\t\t   WELCOME ");
+     printf("\n\t\t\t      to ");
+     printf("\n\t\t\t   THE GAME ");
+     printf("\n\t\t________________________________________");
+     printf("\n\t\t________________________________________");
+     printf("\n\t\t   BECOME A WINNER!!!!!!!!!!!    ") ;
+     printf("\n\t\t________________________________________");
+     printf("\n\t\t________________________________________");
+     printf("\n\t\t > Press S to start the game");
+     printf("\n\t\t > Press V to view the highest score  ");
+     printf("\n\t\t > Press R to reset score");
+     printf("\n\t\t > press H for help            ");
+     printf("\n\t\t > press Q to quit             ");
+     printf("\n\t\t________________________________________\n\n");
+     choice=toupper(getch());
+     if (choice=='V')
+	{
+	show_record();
+	goto mainhome;
+	}
+     else if (choice=='H')
+	{
+	help();getch();
+	goto mainhome;
+	}
+	else if (choice=='R')
+	{reset_score();
+	getch();
+	goto mainhome;}
+	else if (choice=='Q')
+	exit(1);
+    else if(choice=='S')
+    {
+     system("cls");
+
+    printf("\n\n\n\n\n\n\n\n\n\n\t\t\tResister your name:");
+     gets(playername);
+
+    system("cls");
+    printf("\n ------------------  Welcome %s to C Program Quiz Game --------------------------",playername);
+    printf("\n\n Here are some tips you might wanna know before playing:");
+    printf("\n -------------------------------------------------------------------------");
+    printf("\n >> There are 2 rounds in this Quiz Game,WARMUP ROUND & CHALLANGE ROUND");
+    printf("\n >> In warmup round you will be asked a total of 3 questions to test your");
+    printf("\n   knowledge. You are eligible to play the game if you give atleast 2");
+    printf("\n    right answers, otherwise you can't proceed further to the Challenge Round.");
+    printf("\n >> Your game starts with CHALLANGE ROUND. In this round you will be asked a");
+    printf("\n    total of 5 questions. Each right answer will be awarded 5 points");
+    printf("\n    By this way you can win !!!!!..........");
+    printf("\n >> You will be given 4 options and you have to press A, B ,C or D for the");
+    printf("\n    right option.");
+    printf("\n >> Press enter key after every submission to proceed to next");
+    printf("\n >> You will be asked questions continuously, till right answers are given");
+    printf("\n >> No negative marking for wrong answers!");
+    printf("\n\n\t!!!!!!!!!!!!! ALL THE BEST !!!!!!!!!!!!!");
+    printf("\n\n\n Press Y  to start the game!\n");
+    printf("\n Press any other key to return to the main menu!");
+    if (toupper(getch())=='Y')
+		{
+		    goto home;
+        }
+	else
+		{
+        goto mainhome;
+       system("cls");
+       }
+
+     home:
+     system("cls");
+     count=0;
+     for(i=1;i<=3;i++)
+     {
+    system("cls");
+     r1=i;
+
+
+     switch(r1)
+		{
+		case 1:
+		printf("\n\nWhat is three fifth of 100?");
+		printf("\n\nA.3\t\tB.5\n\nC.20\t\tD.60");
+		if (toupper(getch())=='D')
+			{
+			    printf("\n\nCorrect!!!");count++;
+			    getch();
+			    break;
+}
+		else
+		       {
+		           printf("\n\nWrong!!! The correct answer is D.60");
+		           getch();
+		       break;
+		       }
+
+        case 2:
+		printf("\n\n\n If David’s age is 27 years old in 2011. What was his age in 2003?");
+		printf("\n\nA.17\t\tB.37\n\nC.20\t\tD.19");
+		if (toupper(getch())=='D')
+			{printf("\n\nCorrect!!!");count++;
+			getch();
+			break;}
+		else
+		       {printf("\n\nWrong!!! The correct answer is D.19");
+		       getch();
+		       break;}
+
+        case 3:
+		printf("\n\n\nWhat is the remainder of 21 divided by 7?");
+		printf("\n\nA.0\t\tB.21\n\nC.1\t\tD.2");
+		if (toupper(getch())=='A')
+			{printf("\n\nCorrect!!!");count++;
+			getch();
+			break;}
+		else
+		       {printf("\n\nWrong!!! The correct answer is A.0");
+		       getch();
+		       break;}}
+		   }
+
+	if(count>=2)
+	{goto test;}
+	else
+	{
+	system("cls");
+	printf("\n\nSORRY YOU ARE NOT ELIGIBLE TO PLAY THIS GAME, BETTER LUCK NEXT TIME");
+	getch();
+	goto mainhome;
+	}
+     test:
+     system("cls");
+     printf("\n\n\t*** CONGRATULATION %s you are eligible to play the Game ***",playername);
+     printf("\n\n\n\n\t!Press any key to Start the Game!");
+     if(toupper(getch())=='p')
+		{goto game;}
+game:
+     countr=0;
+     for(i=1;i<=5;i++)
+     {system("cls");
+     r=i;
+
+     switch(r)
+		{
+		case 1:
+		printf("\n\nIf you hav 24 apples and you take away 3 how many apples do you have?");
+		printf("\n\nA.7\t\tB.20\n\nC.21\t\tD.10");
+		if (toupper(getch())=='C')
+			{printf("\n\nCorrect!!!");countr++;getch();
+			 break;getch();}
+		else
+		       {printf("\n\nWrong!!! The correct answer is C.21");getch();
+		       goto score;
+		       break;}
+
+		case 2:
+		printf("\n\n\nIn a century how many months are there?");
+		printf("\n\nA.1200\t\tB.12\n\nC.120\t\tD.12000");
+		if (toupper(getch())=='A')
+			{printf("\n\nCorrect!!!");countr++;getch();
+			 break;}
+		else
+		       {printf("\n\nWrong!!! The correct answer is A.1200");getch();
+		      goto score;
+		       break;
+		       }
+
+        case 3:
+		printf("\n\n\nWhat is the reciprocal of 17/15? ");
+		printf("\n\nA.7/5\t\tB.5/7\n\nC.15/17\t\tD.9");
+		if (toupper(getch())=='C')
+			{printf("\n\nCorrect!!!");countr++;getch();
+			 break;}
+		else
+		       {printf("\n\nWrong!!! The correct answer is C.15/17");getch();
+		       goto score;
+		       break;}
+
+        case 4:
+		printf("\n\n\nWhat is the value of x if x2 = 169");
+		printf("\n\nA.13\t\tB.15\n\nC.16990\t\tD.133");
+		if (toupper(getch())=='A')
+			{printf("\n\nCorrect!!!");countr++;getch();
+			 break;}
+		else
+		       {
+                printf("\n\nWrong!!! The correct answer is A.13");getch();
+		       goto score;
+		       break;
+		       }
+
+        case 5:
+		printf("\n\n\nIn what unit is electric power measured?");
+		printf("\n\nA.Coulomb\t\tB.Watt\n\nC.Power\t\tD.Units");
+		if (toupper(getch())=='B')
+			{printf("\n\nCorrect!!!");countr++;getch(); break;}
+		else
+		       {
+		           printf("\n\nWrong!!! The correct answer is B.Power");
+		       getch();
+		       goto score;
+		       break;
+		       }}
+		   }
+
+	score:
+    system("cls");
+	score=(float)countr*5;
+	if(score>0.00 && score<16)
+	{
+	   printf("\n\n\t\t**************** CONGRATULATION *****************");
+	     printf("\n\t You won $%.2f",score);goto go;}
+
+	 else if(score==20)
+	{
+	    printf("\n\n\n \t\t**************** CONGRATULATION ****************");
+	    printf("\n\t\t\t\t YOU ARE THE WINNER!!!!!!!!!");
+	    printf("\n\t\t You won $%.2f",score);
+	    printf("\t\t Thank You!!");
+	}
+	 else
+{
+	 printf("\n\n\t******** SORRY YOU LOST********");
+	    printf("\n\t\t Thanks for Participating");
+	    printf("\n\t\t PLEASE TRY AGAIN");goto go;}
+
+	go:
+	puts("\n\n Press Y to play next game");
+	puts(" Press any key to go main menu");
+	if (toupper(getch())=='Y')
+		goto home;
+	else
+		{
+		edit_score(score,playername);
+		goto mainhome;}}}
+
+void show_record()
+    {system("cls");
+	char name[20];
+	float scr;
+	FILE *f;
+	f=fopen("score.txt","r");
+	fscanf(f,"%s%f",&name,&scr);
+	printf("\n\n\t\t*************************************************************");
+	printf("\n\n\t\t %s has secured the Highest Score %0.2f",name,scr);
+	printf("\n\n\t\t*************************************************************");
+	fclose(f);
+	getch();}
+
+void reset_score()
+    {system("cls");
+    float sc;
+	char nm[20];
+	FILE *f;
+	f=fopen("score.txt","r+");
+	fscanf(f,"%s%f",&nm,&sc);
+	sc=0;
+	fprintf(f,"%s,%.2f",nm,sc);
+    fclose(f);}
+
+void help()
+	{system("cls");
+    printf("\n\n                              HELP");
+    printf("\n -------------------------------------------------------------------------");
+    printf("\n ......................... C program Quiz Game...........");
+    printf("\n >> There are two rounds in the game, WARMUP ROUND & CHALLANGE ROUND");
+    printf("\n >> In warmup round you will be asked a total of 3 questions to test your general");
+    printf("\n    knowledge. You will be eligible to play the game if you can give atleast 2");
+    printf("\n    right answers otherwise you can't play the Game...........");
+    printf("\n >> Your game starts with the CHALLANGE ROUND. In this round you will be asked");
+    printf("\n    total 10 questions each right answer will be awarded $100,000.");
+    printf("\n    By this way you can win upto ONE MILLION cash prize in USD...............");
+    printf("\n >> You will be given 4 options and you have to press A, B ,C or D for the");
+    printf("\n    right option");
+    printf("\n >> You will be asked questions continuously if you keep giving the right answers.");
+    printf("\n >> No negative marking for wrong answers");
+
+	printf("\n\n\t*********************BEST OF LUCK*********************************");
+	printf("\n\n\t*****C PROGRAM QUIZ GAME is developed by PRIYANKA RAI********");}
+
+void edit_score(float score, char plnm[20])
+	{system("cls");
+	float sc;
+	char nm[20];
+	FILE *f;
+	f=fopen("score.txt","r");
+	fscanf(f,"%s%f",&nm,&sc);
+	if (score>=sc)
+	  { sc=score;
+	    fclose(f);
+	    f=fopen("score.txt","w");
+	    fprintf(f,"%s\n%.2f",plnm,sc);
+	    fclose(f);}}
